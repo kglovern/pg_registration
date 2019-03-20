54d48e08a68d6bd9a0f07ef3eb00dfe71040b3cd
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2019-03-20 19:33:47 -0400

    :sparkles: Initial Commit :sparkles:

---
+++ src/BunchLib.py
@@ -0,0 +1,40 @@
+import numpy as np
+import skimage as sk
+import skimage.io as skio
+import skimage.transform as sktx
+import matplotlib.pyplot as plt
+
+def getImgHeight(img):
+    return np.floor(img.shape[0])
+
+def getChannelsFromOrig(img):
+    img = sk.img_as_float(img)
+    imgHeight = getImgHeight(img)
+    
+    sliceHeight = int(np.floor(imgHeight / 3))
+
+    blueEnd = sliceHeight
+    greenEnd = 2 * sliceHeight
+    redEnd = 3 * sliceHeight
+
+    blue = img[:blueEnd]
+    green = img[blueEnd:greenEnd]
+    red = img[greenEnd:redEnd]
+
+    return red, green, blue
+
+def openImg(fPath):
+    return skio.imread(fPath)
+
+
+def colorizeGorskiiImg(fPath):
+    img = openImg(fPath)
+    rc, gc, bc = getChannelsFromOrig(img)
+    plt.figure()
+    combined = np.dstack((rc, gc, bc))
+    skio.imshow(combined)
+    plt.show()
+    plt.clf()
+
+
+
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import BunchLib as bl
+import sys
+import os
+
+def main():
+    if (len(sys.argv) < 2):
+        print("Usage: main.py <image path>")
+    else:
+        imgPath = sys.argv[1]
+        if (os.path.isdir(imgPath)):
+            print("NYI")
+        else:
+            bl.colorizeGorskiiImg(imgPath)
+
+if __name__ == "__main__":
+    main()
