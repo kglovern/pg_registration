7706d286d978c11e20fce9db5da8f3032e67fa84
Kevin Glover-Netherton <kglovern@mail.uoguelph.ca>
2019-03-21 21:46:45 -0400

    Shift added, not working - one channel shifted too high

--- src/BunchLib.py
+++ src/BunchLib.py
@@ -2,6 +2,7 @@ import numpy as np
 import skimage as sk
 import skimage.io as skio
 import skimage.transform as sktx
+from scipy.ndimage import fourier_shift
 import matplotlib.pyplot as plt
 from skimage.measure import compare_ssim as ssim
 import time
@@ -18,10 +19,9 @@ def cropImg(img, amt=0.1):
     pass
 
 def getChannelsFromOrig(img):
-    start = time.time()
     img = sk.img_as_float(img)
     imgHeight = getImgHeight(img)
-    
+
     sliceHeight = int(np.floor(imgHeight / 3))
 
     blueEnd = sliceHeight
@@ -30,10 +30,8 @@ def getChannelsFromOrig(img):
 
     blue = img[:blueEnd]
     green = img[blueEnd:greenEnd]
-    red = img[greenEnd:redEnd]
-    
-    end = time.time()
-    printOperationTime(start, end)
+    red = img[greenEnd:redEnd] # Need redEnd to account for rounding issues leading to one channel being larger than the others
+
 
     return red, green, blue
 
@@ -45,16 +43,20 @@ def getWindow(img, xStart, yStart, width):
 
 def getDownscaledWindow(img, xStart, yStart, width, scale):
     retImg = getWindow(img, xStart, yStart, width)
-    retImg = sktx.rescale(retImg, scale, mode="reflect", anti_aliasing=False, multichannel=False) 
+    retImg = sktx.rescale(retImg, scale, mode="reflect", anti_aliasing=False, multichannel=False)
     return retImg
 
+def shiftImage(img, xOff, yOff):
+    trans = sktx.AffineTransform(translation=(xOff, yOff))
+    return sktx.warp(img, trans)
 
 def alignChannels(ref, target, xOff=0.4, yOff=0.4, wSize=0.38, numMoves=24):
     bestX = 0
     bestY = 0
+    shiftChanged = 0
     maxSSIM = 0
-    pyrLevel = 4
-    
+    pyrLevel = 3
+
     iWidth, iHeight = ref.shape
     xStart = int(xOff * iWidth)
     yStart = int(yOff * iHeight)
@@ -67,13 +69,17 @@ def alignChannels(ref, target, xOff=0.4, yOff=0.4, wSize=0.38, numMoves=24):
     iterationCount = 0
     start = time.time()
     while not curScale > 1:
-        dTarget = getDownscaledWindow(target, xStart, yStart, wWidth, curScale) 
-        for xOff in range(int(-0.5 * numMoves), int(0.5 * numMoves)):
-            for yOff in range (int(-0.5 * numMoves), int(0.5 * numMoves)): 
-                iterationCount += 1
+        print(f"Scale: {curScale}")
+        shiftChanged = False
+        bestX = 0
+        bestY = 0
+        dTarget = getDownscaledWindow(target, xStart, yStart, wWidth, curScale)
+        for yOff in range(int(-0.5 * numMoves), int(0.5 * numMoves)):
+            for xOff in range (int(-0.5 * numMoves), int(0.5 * numMoves)):
                 dRef = getDownscaledWindow(modRef, xStart + xOff, yStart + yOff, wWidth, curScale)
-                curSSIM = ssim(dRef, dTarget)
+                curSSIM = ssim(dTarget, dRef)
                 if curSSIM > maxSSIM:
+                    shiftChanged = True
                     maxSSIM = curSSIM
                     bestX = xOff
                     bestY = yOff
@@ -82,13 +88,13 @@ def alignChannels(ref, target, xOff=0.4, yOff=0.4, wSize=0.38, numMoves=24):
         # Shift original channel
         xShift = bestX * math.pow(2, (pyrLevel - 1))
         yShift = bestY * math.pow(2, (pyrLevel - 1))
-        print(f"Shifts: {xShift}, {yShift}")
-        # DO THE SHIFT
+        if shiftChanged:
+            modRef = shiftImage(modRef, xShift, yShift)
         curScale = 2 * curScale
-    print (f"Iterations: {iterationCount}")
+        pyrLevel -= 1
     end = time.time()
     printOperationTime(start, end)
-    return ref
+    return modRef
 
 def saveImg(img, fPath):
     skio.imsave(fPath, img)
@@ -105,8 +111,13 @@ def showImageAsFigure(img):
 def combineChannels(r, g, b):
     return np.dstack((r, g, b))
 
+def to8Bit(img):
+    return skimage.img_as_ubyte(img)
+
 def colorizeGorskiiImgNaive(fPath):
     img = openImg(fPath)
+    # Downsample from 16 to 8 bit grayscale
+    img = to8Bit(img)
     rc, gc, bc = getChannelsFromOrig(img)
     combined = combineChannels(rc, gc, bc)
     showImageAsFigure(combined)
@@ -116,10 +127,12 @@ def colorizeGorskiiImgWirth(fPath):
     img = openImg(fPath)
     rc, gc, bc = getChannelsFromOrig(img)
     bc = alignChannels(bc, gc)
-    #gc = alignChannels(gc, rc)
+    gc = alignChannels(gc, rc)
+    saveImg (rc, "red.png")
+    saveImg(gc, "green.png")
+    saveImg(bc, "blue.png")
     combined = combineChannels(rc, gc, bc)
-    # TODO: Crop
-    #showImageAsFigure(combined)
+    saveImg(combined, "output.png")
 
 
 
