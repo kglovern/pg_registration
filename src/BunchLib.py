dc3493335c62c795383e2bf9575913c071f6cf2b
Kevin Glover-Netherton <kglovern@mail.uoguelph.ca>
2019-03-26 11:16:30 -0400

    Still overshifting but faster

--- src/BunchLib.py
+++ src/BunchLib.py
@@ -15,6 +15,13 @@ import warnings
 # TOFIX: The low res warnings from conversion.  For now just suppress them
 warnings.filterwarnings("ignore")
 
+def saveImg(img, fPath):
+    directory = "/".join(fPath.split("/")[:-1])
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+    skio.imsave(fPath, img)
+
 
 def getImgHeight(img):
     return np.floor(img.shape[0])
@@ -55,76 +62,64 @@ def openImg(fPath):
     return skio.imread(fPath)
 
 
-def getWindow(img, xStart, yStart, width):
+def getWindow(img, xOff, yOff, wX=0.4, wY=0.4, wSize=0.38):
+    iWidth, iHeight = img.shape
+    xStart = int(wX * iWidth)
+    yStart = int(wY * iHeight)
+    width = int(wSize * iWidth)
     return img[yStart:yStart + width, xStart:xStart + width]
-
-
-def getDownscaledWindow(img, xStart, yStart, width, scale):
-    retImg = getWindow(img, xStart, yStart, width)
-    retImg = sktx.rescale(retImg, scale, mode="reflect", anti_aliasing=False, multichannel=False)
-    return retImg
-
 
 def shiftImage(img, xOff, yOff):
     trans = sktx.AffineTransform(translation=(xOff, yOff))
     return sktx.warp(img, trans)
 
+def getPyramidArray(img, levels):
+    pyramids =  list(sktx.pyramid_gaussian(img, max_layer=(levels-1)))
+    pyramids.reverse()
+    #for i in range(0, levels):
+    #    saveImg(pyramids[i], f"results/{i}_pyr.png")
+    return pyramids
 
-def alignChannels(ref, target, xOff=0.4, yOff=0.4, wSize=0.38, numMoves=24, pyrLevel=4):
+def alignChannels(ref, target, numMoves=24, pyrLevel=4):
     bestX = 0
     bestY = 0
     shiftChanged = False
     maxSSIM = 0
-
-    iWidth, iHeight = ref.shape
-    xStart = int(xOff * iWidth)
-    yStart = int(yOff * iHeight)
-    wWidth = int(wSize * iWidth)
-
-    # Initial downscale at 1/8th resolution - TODO: use Pyrlevel to calculate
-    curScale = 1.0 / 8.0
+    targetPyr = getPyramidArray(target, pyrLevel)
 
     modRef = ref
-    iterationCount = 0
     start = time.time()
-    while not curScale > 1:
-        print(f"Scale: {curScale}")
+    curLevel = pyrLevel
+    for i in range(0, pyrLevel):
+        print(f"Pyramid Level {i+1}")
         shiftChanged = False
         bestX = 0
         bestY = 0
-        dTarget = getDownscaledWindow(target, xStart, yStart, wWidth, curScale)
+        refPyr = getPyramidArray(modRef, pyrLevel) # new Pyramid after each alteration
+        dTarget = getWindow(targetPyr[i], 0, 0)
+        saveImg(dTarget, f"results/{i}_target.png")
         for yOff in range(int(-0.5 * numMoves), int(0.5 * numMoves)):
             for xOff in range(int(-0.5 * numMoves), int(0.5 * numMoves)):
-                dRef = getDownscaledWindow(modRef, xStart + xOff, yStart + yOff, wWidth, curScale)
+                dRef = getWindow(refPyr[i], xOff, yOff)
+                if not shiftChanged:
+                    saveImg(dRef, f"results/{i}_ref.png")
                 curSSIM = ssim(dRef, dTarget)
                 if curSSIM > maxSSIM:
                     shiftChanged = True
                     maxSSIM = curSSIM
                     bestX = xOff
                     bestY = yOff
-                    # print (f"New Max at {xOff}, {yOff}: {maxSSIM}")
-
-        # Shift original channel - pyramids
-        #xShift = bestX * math.pow(2, (pyrLevel - 1))
-        #yShift = bestY * math.pow(2, (pyrLevel - 1))
-        xShift = bestX
-        yShift = bestY
-        # print(f"Shift: {xShift}, {yShift}")
+                    print (f"New Max at {xOff}, {yOff}: {maxSSIM}")
         if shiftChanged:
+            xShift = bestX * math.pow(2, (curLevel - 1))
+            yShift = bestY * math.pow(2, (curLevel - 1))
+            print(f"Best X/Y: {bestX}/{bestY}")
+            print(f"Shifting: {xShift}, {yShift}")
             modRef = shiftImage(modRef, xShift, yShift)
-        curScale = 2 * curScale
-        pyrLevel -= 1
+        curLevel -= 1
     end = time.time()
     opTime = getOperationTime(start, end)
     return modRef, opTime
-
-
-def saveImg(img, fPath):
-    directory = "/".join(fPath.split("/")[:-1])
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-
-    skio.imsave(fPath, img)
 
 
 def showImageAsFigure(img):
@@ -132,6 +127,16 @@ def showImageAsFigure(img):
     skio.imshow(img)
     plt.show()
     plt.clf()
+
+def getFileName(fPath):
+    fileName = fPath.split('/')[-1]
+    return fileName.split('.')[0]
+
+def makeOutputDir(file):
+    filename = getFileName(file)
+    oDir = os.getcwd() + f"/results/{filename}"
+    if not os.path.isdir(oDir):
+        os.mkdir(oDir)
 
 
 def combineChannels(r, g, b):
@@ -152,18 +157,6 @@ def colorizeGorskiiImgNaive(fPath):
     rc, gc, bc = getChannelsFromOrig(img)
     combined = combineChannels(rc, gc, bc)
     saveImg(combined, f"{oDir}/naive.png")
-
-
-def getFileName(fPath):
-    fileName = fPath.split('/')[-1]
-    return fileName.split('.')[0]
-
-
-def makeOutputDir(file):
-    filename = getFileName(file)
-    oDir = os.getcwd() + f"/results/{filename}"
-    if not os.path.isdir(oDir):
-        os.mkdir(oDir)
 
 
 def colorizeGorskiiImgWirth(fPath):
